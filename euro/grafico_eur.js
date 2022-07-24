3fc6596d34cb98ff89ad8a94ff4e4f9974894945
Mateus444 <mateus20040914@gmail.com>
2022-07-24 14:14:04 -0300

    Arquivos do dolar-agora

---
+++ euro/grafico_eur.js
@@ -0,0 +1,69 @@
+function grafico() {
+  const url30dias = "https://economia.awesomeapi.com.br/json/daily/EUR-BRL/30";
+  const xhttp = new XMLHttpRequest();
+
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      const retorno = xhttp.responseText;
+      const retornoParaObj = JSON.parse(retorno);
+      
+      //formatar as cotações
+      const cotacoes = [];
+      const cotacoesFormatadas = []
+
+      for (let i = 0; i < 30; i++) {
+        cotacoes[i] = retornoParaObj[i].bid
+        cotacoesFormatadas[i] = parseFloat(cotacoes[i]).toFixed(2)
+      }
+
+      //guarda os timestamp de todos 30 dias
+      const timestamp = [];
+      const datas = []
+      
+      for (let i = 0; i < 30; i++) {
+        timestamp[i] = retornoParaObj[i].timestamp;
+        datas[i] = new Date(timestamp[i] * 1000)
+      }
+      
+      //guarda as datas formatadas
+      const datasFormatadas = []
+      
+      for (let i = 0; i < 30; i++) {
+        datasFormatadas[i] = ("00"+datas[i].getDate()).slice(-2)+"/"+("00"+(datas[i].getMonth()+1)).slice(-2)+"/"+datas[i].getFullYear()
+      }
+      
+      //Inverter a ordem das datas e das cotações para ficar correto no gráfico 
+      datasFormatadas.reverse()
+      cotacoesFormatadas.reverse()
+  
+      //Desenhar o grafico
+      var ctx = document.getElementById('myChart');
+      Chart.defaults.font.size = 11;
+      Chart.defaults.color = '#808080';
+      const cotGraficco = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: datasFormatadas,
+          datasets: [{
+            label: "Cotação",
+            pointRadius: 0,
+            data: cotacoesFormatadas,
+            borderWidth: 2,
+            borderColor: "#1260CC",
+						  	  }]
+        },
+        options: {
+          interaction: {
+            intersect: false,
+            mode: 'index',
+          },
+        }
+
+      });
+    }
+  };
+
+  xhttp.open("GET", url30dias, true);
+  xhttp.send();
+}
+grafico()
---
+++ iene-japones/jpy.js
@@ -0,0 +1,49 @@
+const moedasUrl = "https://economia.awesomeapi.com.br/json/last/JPY-BRL"
+const http = new XMLHttpRequest()
+
+function pegarValor(resposta) {
+  const resCotacao = resposta
+  const cotacaoObj = JSON.parse(resCotacao)
+  const preco = cotacaoObj.JPYBRL.bid
+  const precoFloat = parseFloat(preco)
+  return precoFloat.toFixed(2)
+}
+
+function atualizarInput(cotacao) {
+  const input1 = document.getElementById("entrada-1")
+  const input2 = document.getElementById("entrada-2")
+  input2.value = cotacao
+  document.getElementById("atual").innerHTML = cotacao
+  input1.addEventListener("keyup", function() {
+    const resultado = input1.value * cotacao
+    input2.value = resultado.toFixed(2)
+  })
+  input2.addEventListener("keyup", function() {
+    const resultado = input2.value / cotacao
+    input1.value = resultado.toFixed(2)
+  })
+}
+
+function atualizarData(resposta) {
+  const returnString = resposta
+  const returnObg = JSON.parse(returnString)
+  const dataTime = returnObg.JPYBRL.timestamp
+  const dataParaInt = parseInt(dataTime)
+  const dataFormatada = new Date(dataParaInt * 1000)
+  const dia = ("00"+dataFormatada.getDate()).slice(-2)
+  const mes = ("00"+(dataFormatada.getMonth()+1)).slice(-2)
+  const ano = dataFormatada.getFullYear()
+  document.getElementById("data").innerHTML = dia+"/"+mes+"/"+ano
+  return dia+"/"+mes+"/"+ano
+}
+
+http.onreadystatechange = function cotacao() {
+  if (http.status == 200 && http.readyState == 4) {
+    const preco = pegarValor(http.responseText)
+    atualizarInput(preco)
+    atualizarData(http.responseText)
+  }
+}
+
+http.open("GET", moedasUrl, true)
+http.send()
